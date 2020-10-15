5ad4d70c653a807aa77456c7003af918ba57d45f
alaedinnemarzougui <alaedinne.marzougui@gmail.com>
2020-10-15 16:14:31 +0200

    refactoring cinfig_app_name

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-        <h2> What is Laracarte !</h2>
+        <h2> What is {{ config('app.name') }} !</h2>
         <p>Laracarte is a clone app of <a href="https://Laramap.com" target="_blank">Laramap.com</a> </p>
 
     <div class="row">
@@ -20,10 +20,10 @@
     <hr>
 
     <h2> What is Laramap ?</h2>
-    <p>Laramap is the website by which Laracarte was inspired :)</p>
+    <p>Laramap is the website by which {{ config('app.name') }} was inspired :)</p>
     <p>More info  <a href="#">Here</a></p>
     <hr>
-    <h2>Which tools and services are used in Laracarte?</h2>
+    <h2>Which tools and services are used in {{ config('app.name') }}?</h2>
     <p> Which it's built on laravel & bootstrap. But there's a bunch of services used for email and other sections.</p>
     <ul>
         <li>Laravel</li>
--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="footer mt-auto py-3 text-center mt-4">
     <div class="container">
-        <span class="text-muted">This app has been built for learning purposes : &copy; {{ date('Y') }} &middot; Laracarte by <a href="#">Alaedinne Marzougui</a></span>
+        <span class="text-muted">This app has been built for learning purposes : &copy; {{ date('Y') }} &middot; {{ config('app.name') }} by <a href="#">Alaedinne Marzougui</a></span>
     </div>
 </footer>
