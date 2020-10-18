4bdc04a59949e8736698e41e0162f1c328413552
alaedinnemarzougui <alaedinne.marzougui@gmail.com>
2020-10-18 12:06:06 +0200

    refactoring page_title

--- app/helpers.php
+++ app/helpers.php
@@ -7,13 +7,8 @@
 
             $base_title =   config('app.name') .' - List of artisans';
 
-            if($title == '') {
+              return  empty($title) ? $base_title : $title .' | '. $base_title;
 
-                 return $base_title ;
-
-            }
-
-            return $title .' | '. $base_title;
 
       }
   }
